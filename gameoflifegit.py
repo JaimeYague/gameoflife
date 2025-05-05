b4099fba91f340aaf1f5026d2e72b7ccf5b6dc7e
Raquel <leuqar999@gmail.com>
2025-05-05 13:38:37 +0200

    terminado

--- gameoflifegit.py
+++ gameoflifegit.py
@@ -1,16 +1,15 @@
 #raquel
-
 import os
 import random
+import time
+
+print(os.get_terminal_size())
+
 
 def cuadricula():
-    filas = 5
-    columnas = 5
+    filas = os.get_terminal_size().lines
+    columnas = os.get_terminal_size().columns
     tablero = [[random.randint(0, 1) for _ in range(columnas)] for _ in range(filas)]
-
-    for fila in tablero:
-        print(fila)
-    
     return tablero
 
 def contarvecinos(tablero, fila, columna):
@@ -18,39 +17,54 @@ def contarvecinos(tablero, fila, columna):
     columnas = len(tablero[0])
     vecinos_vivos = 0
     
-    for i in range(fila -1, fila +2):
-        for j in range(columna -1, columna +2):
+    for i in range(fila - 1, fila + 2):
+        for j in range(columna - 1, columna + 2):
             if i == fila and j == columna:
                 continue
 
             if 0 <= i < filas and 0 <= j < columnas:
                 vecinos_vivos += tablero[i][j]
 
-        return vecinos_vivos
-    
-def tablero_nuevo(tablero):
+    return vecinos_vivos
+
+
+def siguiente_generacion(tablero):
     filas = len(tablero)
-    columnas = len(tablero[0])    
+    columnas = len(tablero[0])
     nuevo_tablero = [[0 for _ in range(columnas)] for _ in range(filas)]
-    celda = tablero[i][j]
-        
+
     for i in range(filas):
         for j in range(columnas):
             vecinos = contarvecinos(tablero, i, j)
-        if celda == 1:
-            if vecinos in [2, 3]:
-                nuevo_tablero[i][j] = 1
+            celda = tablero[i][j]
+
+            # Reglas del Juego de la Vida
+            if celda == 1:
+                if vecinos == 2 or vecinos == 3:
+                    nuevo_tablero[i][j] = 1  # sobrevive
+                else:
+                    nuevo_tablero[i][j] = 0  # muere
             else:
-                nuevo_tablero[i][j] = 0
-        else:
-            if vecinos == 3:
-                nuevo_tablero[i][j] = 1
+                if vecinos == 3:
+                    nuevo_tablero[i][j] = 1  # nace
+                else:
+                    nuevo_tablero[i][j] = 0  # sigue muerta
+
     return nuevo_tablero
 
 
-tablero = cuadricula
+tablero = cuadricula()
+
+while True:
+    os.system('cls' if os.name == 'nt' else 'clear')  # limpia pantalla
+    for fila in tablero:
+        print("".join("█" if celda else " " for celda in fila))
+    tablero = siguiente_generacion(tablero)
+    time.sleep(0.5)
 
 
+
+    
 
 
 
